b9a3a88795c9709d5eea209ed51872844f9cbe47
Alexander,Terry T <s524729@ch112025m.nwmissouri.edu>
2018-03-14 11:50:46 -0500

    Got location services working, I need to figure out how to enable location services working without allowing always and then exiting. But current status is working.

--- Designated Driver/ViewController.swift
+++ Designated Driver/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import CoreLocation
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var textView: UITextView!
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startReceivingLocationChanges()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func startReceivingLocationChanges() {
+        // Get authorization details
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        if authorizationStatus != .authorizedWhenInUse && authorizationStatus != .authorizedAlways {
+            //can't continue because user didn't authorize the location services
+            //provide popup in case they forgot
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        //start services if it hasn't been yet
+        if !CLLocationManager.locationServicesEnabled() {
+            //tell users to start the service or provide popup
+            
+            return
+        }
+        
+        //configure the service
+        
+        //desired accuracy 100m
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100.0
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        let currentLocation = locationManager.location
+        textView.text = locationToString(currentLocation!)
 
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation = locations.last!
+        //display this to the screen
+        print("Current Location: \(currentLocation)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("There was an error. \(error)")
+    }
+    
+    func locationToString(_ loc:CLLocation)->String{
+        var str = ""
+        let coord = loc.coordinate
+        str += "Latitude: \(coord.latitude) Longitude: \(coord.longitude)"
+        return str
+    }
 
 }
 
